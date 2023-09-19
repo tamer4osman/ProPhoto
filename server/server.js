0a2b92c2cccf7a20619ce2378db77319283d8d03
Tamer Osman <tamer4osman@gmail.com>
2023-09-19 18:13:10 +0200

    Create Storage feature

--- server/server.js
+++ server/server.js
@@ -1,21 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const bodyParser =require ("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const path = require("path");
-const { fileURLToPath } = require("url");
+const { fileURLToPath, URL } = require("url");
 
 const app = express();
 
-// middleware
+// Remove the explicit declaration of __filename and __dirname
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({ policy: "cross-origin" })); // for security
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+
 require("dotenv").config();
+
+const store = multer.diskStorage({ 
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: store }).single("file");
 
 const port = process.env.PORT || 3000;
 // database connection
@@ -27,7 +49,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then((result) =>
+  .then(() =>
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     })
