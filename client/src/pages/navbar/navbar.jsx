22f7037e539e434dc0d34b474b23872ec8f02b8b
Tamer Osman <tamer4osman@gmail.com>
2023-09-29 10:59:39 +0200

    Fix some errors resulting from incorrect deletion

--- client/src/pages/navbar/navbar.jsx
+++ client/src/pages/navbar/navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          ProPhoto
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
