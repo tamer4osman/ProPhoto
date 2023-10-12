80a1b719d5de3bbf811799a4a2cd6fd42786555e
Tamer Osman <tamer4osman@gmail.com>
2023-10-12 22:00:02 +0200

    Refactoring Posts Widget

--- client/src/pages/widgets/PostsWidget.jsx
+++ client/src/pages/widgets/PostsWidget.jsx
@@ -5,69 +5,48 @@ import PostWidget from "./PostWidget";
 
 const PostsWidget = ({ userId, isProfile = false }) => {
   const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts);
-  const token = useSelector((state) => state.token);
+  const { posts, token } = useSelector((state) => state);
 
-  const getPosts = async () => {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/posts`, {
+  const fetchData = async (url) => {
+    const response = await fetch(url, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
-    const data = await response.json();
+    return await response.json();
+  };
+
+  const getPosts = async () => {
+    const data = await fetchData(`${import.meta.env.VITE_API_URL}/posts`);
     dispatch(setPosts({ posts: data }));
   };
 
   const getUserPosts = async () => {
-    const response = await fetch(
-      `${import.meta.env.VITE_API_URL}/posts/${userId}/posts`,
-      {
-        method: "GET",
-        headers: { Authorization: `Bearer ${token}` },
-      }
+    const data = await fetchData(
+      `${import.meta.env.VITE_API_URL}/posts/${userId}/posts`
     );
-    const data = await response.json();
     dispatch(setPosts({ posts: data }));
   };
 
   useEffect(() => {
-    if (isProfile) {
-      getUserPosts();
-    } else {
-      getPosts();
-    }
+    isProfile ? getUserPosts() : getPosts();
   }, []);
 
   return (
     <>
-      {!posts ? (
-        <p>Loading</p>
-      ) : (
-        posts.map(
-          ({
-            _id,
-            userId,
-            firstName,
-            lastName,
-            description,
-            location,
-            picturePath,
-            userPicturePath,
-            likes,
-            comments,
-          }) => (
-            <PostWidget
-              key={_id}
-              postId={_id}
-              postUserId={userId}
-              name={`${firstName} ${lastName}`}
-              description={description}
-              // location={location}
-              picturePath={picturePath}
-              userPicturePath={userPicturePath}
-              likes={likes}
-              comments={comments}
-            />
-          )
+      {!posts ? <p>Loading</p> : (
+        posts.map(({ _id, userId, firstName, lastName, description, location, picturePath, userPicturePath, likes, comments }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />)
         )
       )}
     </>
