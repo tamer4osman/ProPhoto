2d0827933a124b589b295223cf5bcbbbffcfcb05
Tamer Osman <tamer4osman@gmail.com>
2023-10-24 18:30:19 +0200

    Refactoring server.js

--- server/server.js
+++ server/server.js
@@ -14,26 +14,25 @@ const { createPost } = require("./controllers/postController");
 const { verifyToken } = require("./middleware/authMiddleware");
 const User = require("./models/User");
 const Post = require("./models/Post");
-const {users, posts} = require("./data/data.js");
+const { users, posts } = require("./data/data.js");
 
 const app = express();
 
-
-
+// Middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-app.use(helmet.contentSecurityPolicy({ policy: "cross-origin" })); // for security
-app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(helmet.contentSecurityPolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-
 require("dotenv").config();
 
-const storage = multer.diskStorage({ 
+// Multer configuration
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/assets");
   },
@@ -44,19 +43,18 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// routes with files to upload
-app.post("/auth/signUp", upload.single("picture"), signUp);
-app.post("/posts", verifyToken,upload.single("picture"), createPost);
+// Routes
+const withFileUpload = (path, handler) => app.post(path, verifyToken, upload.single("picture"), handler);
 
-// routes without files to upload
+withFileUpload("/auth/signUp", signUp);
+withFileUpload("/posts", createPost);
 
 app.use("/auth", authRoutes);
-app.use("/users",userRoutes);
-app.use("/posts",postRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
+// Server setup
 const port = process.env.PORT || 6001;
-// database connection
-
 const dbURI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}${process.env.MONGODB_URL}/${process.env.MONGODB_DATABASE}`;
 
 mongoose
@@ -64,12 +62,11 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() =>
+  .then(() => {
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
       // User.insertMany(users);
       // Post.insertMany(posts);
-
-    })
-  )
+    });
+  })
   .catch((err) => console.log(err));
